8b160a94f1e9959bfa7a45e4cc39159389ee990b
MostafaMofeed67 <mofeedmostafa83@gmail.com>
2022-05-05 03:13:30 +0200

    adding services form

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,164 +1,57 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import Header from "../../components/Header";
-
-import formBtnImg from "../../images/formBtn.png";
-import logoImg from "../../images/logo.png";
-import bgImg from "../../images/bgImg.png";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import exitBtn from "../../images/exitBtn.png";
+import homeBtn from "../../images/homeBtn.png";
+import bgImg2 from "../../images/Hospital-Case-S.png";
+import ServicesForm from "./ServicesForm";
 
 const Services = () => {
-  const navigate = useNavigate();
+  const [showForm, setShowForm] = useState(false);
 
-  const submitFormHandler = (e) => {
-    e.preventDefault();
+  const showFormHandler = () => setShowForm(true);
 
-    navigate("/");
-  };
+  const hideFormHandler = () => setShowForm(false);
 
-  const navigateToHome = () => {
-    navigate("/");
-  };
+  if (showForm) {
+    return <ServicesForm hideFormHandler={hideFormHandler} />;
+  }
 
   return (
     <>
-      <div className="absolute w-full min-h-screen z-10">
-        <img
-          src={bgImg}
-          alt=""
-          className="absolute w-full h-[100%] object-cover z-10"
-        />
+      <img src={bgImg2} alt="" className="h-96 w-full object-cover" />
+
+      <div className="max-w-5xl w-full mx-auto mt-11 rounded-md p-3 relative border-[#06bac0] border border-solid text-center">
+        <h1 className="text-xl font-extrabold absolute -top-4 px-3  right-10 bg-white">
+          الإستعلام عن المريض
+        </h1>
+
+        <div className="flex flex-col">
+          <input
+            type="text"
+            id="name"
+            placeholder="أدخل الرقم القومي"
+            className="w-[50%] mt-8  p-3 mx-auto rounded-xl h-11 text-xl  focus:outline-none shadow-sm shadow-[#06bac0]"
+          />
+          <input
+            type="text"
+            id="name"
+            placeholder="رقم بطاقه التأمين"
+            className="w-[50%] mt-7  mb-14  p-3 mx-auto rounded-xl h-11 text-xl  focus:outline-none shadow-sm shadow-[#06bac0]"
+          />
+        </div>
+
+        <div className="flex justify-between px-5">
+          <img
+            src={exitBtn}
+            alt=""
+            className="w-10 cursor-pointer"
+            onClick={showFormHandler}
+          />
+          <Link to="/">
+            <img src={homeBtn} alt="" className="w-10 cursor-pointer" />
+          </Link>
+        </div>
       </div>
-
-      <Header />
-
-      <div
-        onClick={navigateToHome}
-        className="absolute inset-0  z-10"
-        style={{ background: "rgba(2,25,37,0.5411764705882353 )" }}
-      ></div>
-
-      <div
-        className="absolute top-[50%] left-[50%] bg-white font-black  z-20 rounded-lg text-primary py-5 px-6 w-4/5"
-        style={{ transform: "translate(-50%,-50%)" }}
-      >
-        <h1 className="text-center mb-5 text-3xl">بيانات المريض</h1>
-
-        <form
-          className="text-2xl flex flex-col gap-5"
-          onSubmit={submitFormHandler}
-        >
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-8">
-              <label htmlFor="name" className="w-[90px]">
-                الاسم
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-            <div className="flex items-center gap-8">
-              <label htmlFor="name">الرقم القومي</label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-8">
-              <label htmlFor="name" className="w-[90px]">
-                السن
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-            <div className="flex items-center gap-8">
-              <label htmlFor="name">الرقم التأميني</label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-8">
-              <label htmlFor="name" className="w-[90px]">
-                النوع
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-            <div className="flex items-center gap-8">
-              <label htmlFor="name">رقم التليفون</label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-8">
-              <label htmlFor="name" className="w-[90px]">
-                العنوان{" "}
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-            <div className="flex items-center gap-8">
-              <label htmlFor="name">فصيلة الدم </label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-8">
-              <label htmlFor="name" className="w-[90px]">
-                الوظيفة{" "}
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-            <div className="flex items-center gap-8">
-              <label htmlFor="name">ملف الحالة الصحية</label>
-              <input
-                type="text"
-                id="name"
-                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
-              />
-            </div>
-          </div>
-
-          <button className="flex justify-center w-fit self-center mt-5">
-            <img src={formBtnImg} alt="" className="w-11" />
-          </button>
-        </form>
-      </div>
-
-      <img src={logoImg} alt="" className="absolute bottom-0 z-30 left-40" />
     </>
   );
 };
---
+++ src/pages/Services/ServicesForm.js
@@ -0,0 +1,161 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../../components/Header";
+import bgImg from "../../images/bgImg.png";
+import formBtnImg from "../../images/formBtn.png";
+import logoImg from "../../images/logo.png";
+
+const ServicesForm = ({ hideFormHandler }) => {
+  const navigate = useNavigate();
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div className="absolute w-full min-h-screen z-10">
+        <img
+          src={bgImg}
+          alt=""
+          className="absolute w-full h-[100%] object-cover z-10"
+        />
+      </div>
+
+      <Header />
+
+      <div
+        onClick={hideFormHandler}
+        className="absolute inset-0  z-10"
+        style={{ background: "rgba(2,25,37,0.5411764705882353 )" }}
+      ></div>
+
+      <div
+        className="absolute top-[50%] left-[50%] bg-white font-black  z-20 rounded-lg text-primary py-5 px-6 w-4/5"
+        style={{ transform: "translate(-50%,-50%)" }}
+      >
+        <h1 className="text-center mb-5 text-3xl">بيانات المريض</h1>
+
+        <form
+          className="text-2xl flex flex-col gap-5"
+          onSubmit={submitFormHandler}
+        >
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-8">
+              <label htmlFor="name" className="w-[90px]">
+                الاسم
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+            <div className="flex items-center gap-8">
+              <label htmlFor="name">الرقم القومي</label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-8">
+              <label htmlFor="name" className="w-[90px]">
+                السن
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+            <div className="flex items-center gap-8">
+              <label htmlFor="name">الرقم التأميني</label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-8">
+              <label htmlFor="name" className="w-[90px]">
+                النوع
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+            <div className="flex items-center gap-8">
+              <label htmlFor="name">رقم التليفون</label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-8">
+              <label htmlFor="name" className="w-[90px]">
+                العنوان{" "}
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+            <div className="flex items-center gap-8">
+              <label htmlFor="name">فصيلة الدم </label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-8">
+              <label htmlFor="name" className="w-[90px]">
+                الوظيفة{" "}
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+            <div className="flex items-center gap-8">
+              <label htmlFor="name">ملف الحالة الصحية</label>
+              <input
+                type="text"
+                id="name"
+                className="w-64 border-2 border-primary rounded-xl h-12 text-base px-3 focus:outline-none"
+              />
+            </div>
+          </div>
+
+          <button className="flex justify-center w-fit self-center mt-5">
+            <img src={formBtnImg} alt="" className="w-11" />
+          </button>
+        </form>
+      </div>
+
+      <img src={logoImg} alt="" className="absolute bottom-0 z-30 left-40" />
+    </>
+  );
+};
+
+export default ServicesForm;
